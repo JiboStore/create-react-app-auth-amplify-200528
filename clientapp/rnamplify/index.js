dcb1f6a1c9e5aa52a9577a7948880d6137ed9799
Hakim Hauston <hakim.hauston@storms.com>
2020-04-29 12:04:06 +0800

    [react-native] init
    -. https://dev.to/aws/the-complete-react-native-guide-to-user-authentication-with-the-amplify-framework-ib2

---
+++ clientapp/rnamplify/index.js
@@ -0,0 +1,16 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
+
+/**
+Hosted UI Endpoint: https://rnamplify85d16374-85d16374-local.auth.ap-southeast-1.amazoncognito.com/
+Test Your Hosted UI Endpoint: https://rnamplify85d16374-85d16374-local.auth.ap-southeast-1.amazoncognito.com/login?response_type=code&client_id=7g1057n1igvpbnvb94hb2hpabo&redirect_uri=myapp://
+https://rnamplify85d16374-85d16374-local.auth.ap-southeast-1.amazoncognito.com/oauth2/idpresponse
+http://localhost:3000/login/callback, 
+**/
